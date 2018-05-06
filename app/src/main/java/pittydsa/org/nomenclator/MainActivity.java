3f926f18b6f6d3b0720814911c88585d544e70ba
David Ankin <daveankin@gmail.com>
2018-05-06 09:27:27 -0400

    less inflating, extra refs, more perf, asynctasks, individual send btns

--- app/src/main/java/pittydsa/org/nomenclator/MainActivity.java
+++ app/src/main/java/pittydsa/org/nomenclator/MainActivity.java
@@ -1,6 +1,7 @@
 package pittydsa.org.nomenclator;
 
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,14 +14,15 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-import static pittydsa.org.nomenclator.Person.people;
-
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linearLayout = (LinearLayout) findViewById(R.id.roster);
     }
 
     /**
@@ -34,9 +36,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendAll(View view) {
-        Person[] people = Person.people;
         String[] permissions = new String[] {Manifest.permission.SEND_SMS};
-
 
         if (ContextCompat.checkSelfPermission(this,
             Manifest.permission.SEND_SMS) !=
@@ -47,26 +47,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-     public void sendAllHavePermissions() {
-        SmsManager smsManager = SmsManager.getDefault();
-         for (Person p :
-                 Person.people) {
-             ArrayList<String> parts = smsManager.divideMessage(p.toString());
-             smsManager.sendMultipartTextMessage(p.getPhoneNumber(), null, parts, null, null);
-         }
-     }
-
     @Override
     public void onRequestPermissionsResult(int requestCode,
             String permissions[], int[] grantResults) {
-        
         if (grantResults.length > 0
             && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             sendAllHavePermissions();
         }
     }
 
-//    private String formatMessage(Person person, String message)
+    public void sendAllHavePermissions() {
+        SmsManager smsManager = SmsManager.getDefault();
+        if (linearLayout == null)
+            linearLayout = (LinearLayout) findViewById(R.id.roster);
+
+        int noPeople = linearLayout.getChildCount();
+        for (int i = 0; i < noPeople; i++) {
+            SingleMessageView s = (SingleMessageView) linearLayout.getChildAt(i);
+            s.sendSingleMessage();
+        }
+    }
 
     /**
      * This method is public because it is safe. It takes an array of people
@@ -74,8 +74,9 @@ public class MainActivity extends AppCompatActivity {
      * adds them into the linearLayout.
      */
     public void showPeopleInRoster(Person[] people) {
-        // find view so we know where to draw the SingleMessageView
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.roster);
+        if (linearLayout == null)
+            linearLayout = (LinearLayout) findViewById(R.id.roster);
+
         for (Person person : people) {
             View view = inflateSMV(person);
             linearLayout.addView(view);
@@ -83,15 +84,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private View inflateSMV(Person person) {
-        // find view so we know how big to draw it
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.roster);
-        View sMV = getLayoutInflater().inflate(R.layout.single_message_view, linearLayout, false);
+        if (linearLayout == null)
+            linearLayout = (LinearLayout) findViewById(R.id.roster);
 
-        //
-        Button button      = (Button) sMV.findViewById(R.id.button);
-        TextView textView  = (TextView) sMV.findViewById(R.id.textView);
-        TextView textView2 = (TextView) sMV.findViewById(R.id.textView2);
-        TextView textView3 = (TextView) sMV.findViewById(R.id.textView3);
+        SingleMessageView sMV = (SingleMessageView) getLayoutInflater()
+                .inflate(R.layout.single_message_view, linearLayout, false);
+        sMV.init();
+
+        Button button      = sMV.getButton();
+        TextView textView  = sMV.getTextView();
+        TextView textView2 = sMV.getTextView2();
+        TextView textView3 = sMV.getTextView3();
 
         // button.setText();
         textView.setText(person.getName());
--- app/src/main/java/pittydsa/org/nomenclator/SingleMessageView.java
+++ app/src/main/java/pittydsa/org/nomenclator/SingleMessageView.java
@@ -1,44 +1,102 @@
 package pittydsa.org.nomenclator;
 
 import android.content.Context;
-import android.support.constraint.ConstraintLayout;
+import android.os.AsyncTask;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 /**
  * Created by toor on 12/19/17.
  */
 
-public class SingleMessageView extends ConstraintLayout {
+public class SingleMessageView extends RelativeLayout {
     private Button button;
     private TextView textView;
     private TextView textView2;
+    private TextView textView3;
+    private AsyncTask<Object, Void, String> sendMessage;
 
     public SingleMessageView(Context context) {
         super(context);
-        init();
     }
 
     public SingleMessageView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
     }
 
     public SingleMessageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        init();
     }
 
-    private void init() {
-        inflate(getContext(), R.layout.single_message_view, this);
+    public void init() {
         button = findViewById(R.id.button3);
         textView = findViewById(R.id.textView);
         textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        sendMessage = new SendMessage();
+
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendSingleMessage();
+            }
+        });
+    }
+
+    public void sendSingleMessage() {
+        button.setEnabled(false);
+        sendMessage.execute();
+    }
+
+    private MainActivity parent = null;
+
+    public void assignButtonClick(MainActivity p) {
+        this.parent = p;
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                parent = null;
+            }
+        });
     }
 
     public void setPersonDetail(Person person) {
         button.setText(person.getName());
         textView.setText(person.getPhoneNumber());
+    }
+
+    public Button getButton() {
+        return button;
+    }
+
+    public TextView getTextView() {
+        return textView;
+    }
+
+    public TextView getTextView2() {
+        return textView2;
+    }
+
+    public TextView getTextView3() {
+        return textView3;
+    }
+
+    private class SendMessage extends AsyncTask<Object, Void, String> {
+        @Override
+        protected String doInBackground(Object... strings) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            button.setText("Sent");
+        }
     }
 }
